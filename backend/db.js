939c82fb17dad2ea1b4f190b5e88f21705ff64da
Dhrimanti <160819524+Dhrimanti@users.noreply.github.com>
2024-07-28 22:03:45 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://dhritimancg:badmos@cluster0.qnkkxjb.mongodb.net/badmos")
+
 
 const todoSchema=mongoose.Schema({
     title:String,
